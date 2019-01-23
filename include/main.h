6e50c7c31ecf4d78783d0a1c82129d559cc53fc2
nickschatz <nick@nickschatz.com>
2019-01-23 00:21:25 -0600

    Feature complete code
    Implement PWM on output pins. Switch to better method of timekeeping for input. Implement output LEDs.

--- include/main.h
+++ include/main.h
@@ -1,20 +1,31 @@
 #pragma once
 
 /*
- * Red LED: PB2
+ * Red LED: PA3
  * Green LED: PA6
  * Input PWM: ~PA0
- * Brake: PA1 -> Low (activate internal pullup)
- * Coast: PA5 -> Low (activate internal pullup)
- * IN1: PA2
- * IN2: PA3
+ * Brake: PA1 -> Low (activate internal pullup), otherwise Coast
+ * IN1: PB2
+ * IN2: PA7
  * NSLEEP: PA4 (pull high to turn on driver)
  */
 
 #define SET_HIGH(register, bit) register |= 1 << bit;
 #define SET_LOW(register, bit) register &= ~(1 << bit);
-#define REGISTER_BIT(register, bit) (register >> bit) & 1;
+#define REGISTER_BIT(register, bit) ((register >> bit) & 1)
+#define SET_REGISTER(register, bit, value) register ^= (-value ^ register) & (1 << bit);
 
-// Number of 5 uS counts of no signal to wait before disabling output
-// 8000 is two signal periods
-#define DEAD_TIME 8000
+// Number of 8.192 ms counts of no signal to wait before disabling output
+#define DEAD_TIME 5
+
+// Smallest number of real timer counts to register an input (1000 uS)
+#define MIN_PULSE_LEN 8000
+
+// Largest number of counts to register an input (2000 uS)
+#define MAX_PULSE_LEN 16000
+
+#define DEADZONE_LOW 11800
+#define DEADZONE_CENTER 12000
+#define DEADZONE_HIGH 12200
+
+#define IN_RANGE 3800
--- src/main.c
+++ src/main.c
@@ -7,49 +7,105 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 
-volatile unsigned short in_low_counts = 0;
-volatile char input_off_latch = 1;  // Records whether no signal has been received for more than DEAD_TIME
-
+volatile unsigned char compare_level = 0;
+volatile char direction = 0;
+volatile unsigned char outer_time = 0;
 int main() {
-    // Configure system clock prescaler (0001 is factor of 2 for 4 MHz operation)
+    // Configure system clock prescaler (0000 is factor of 1 for 8 MHz operation)
     CCP = 0xD8;
-    CLKPSR |= 0b0001;
+    CLKPSR &= 0b0000;
 
     // Set timer
     SET_HIGH(TCCR1A, CS10) // 16 bit timer, prescale of 1
-    // Set output compare register
-    OCR1A = 19; // 5 uS resolution
+    SET_HIGH(TIMSK, TOIE1) // Enable overflow interrupt (used for keeping longer time)
 
-
-    // Configure pin change interrupt on PA0
+    // Input PWM signal
+    // Configure pin change interrupt on PA0 
     SET_HIGH(PCMSK0, PCINT0)
     SET_HIGH(GIMSK, PCIE0)
-    // Configure A0 as input (logic low)
+    // Configure A0 as input
     SET_LOW(DDRA, DDA0)
     // Disable pullup resistor (set high-Z)
     SET_LOW(PUEA, PUEA0)
 
+    // Configure output PWM timer (phase correct PWM, output when timer above level)
+    SET_LOW(TCCR0B, WGM02)
+    SET_LOW(TCCR0B, WGM01)
+    SET_HIGH(TCCR0B, WGM00)
+
+    // These stay low all the time
+    SET_LOW(TCCR0A, COM0B0)
+    SET_LOW(TCCR0A, COM0A0)
+
     sei(); // Enable interrupts
 
-    // Configure PA1 and PA5 as internal-pullup logic inputs
+    // Brake/coast jumper
+    // Configure PA1 as internal-pullup logic inputs
     SET_LOW(DDRA, DDA1)
     SET_HIGH(PUEA, PUEA1)
-    SET_LOW(DDRA, DDA5)
-    SET_HIGH(PUEA, PUEA5)
 
-    // Configure PB2 and PA6 as outputs and disable pullups
+    // LED outputs
+    // Configure PA3 and PA6 as outputs and disable pullups
     SET_HIGH(DDRA, DDA6)
     SET_LOW(PUEA, PUEA6)
+    SET_HIGH(DDRA, DDA3)
+    SET_LOW(PUEA, PUEA3)
+
+    // IN1, IN2, NSLEEP setup
+    // Configure PA4,7, PB2 as outputs
+    SET_HIGH(DDRA, DDA4)
+    SET_LOW(PUEA, PUEA4)
+    SET_HIGH(DDRA, DDA7)
+    SET_LOW(PUEA, PUEA7)
     SET_HIGH(DDRB, DDB2)
     SET_LOW(PUEB, PUEB2)
 
-    // Configure PA2,3,4 as outputs
-    SET_HIGH(DDRA, DDA2)
-    SET_LOW(PUEA, PUEA2)
-    SET_HIGH(DDRA, DDA3)
-    SET_LOW(PUEA, PUEA3)
-    SET_HIGH(DDRA, DDA4)
-    SET_LOW(PUEA, PUEA4)
+    // Default output state
+    SET_LOW(PINA, PINA4) // NSLEEP
+    SET_LOW(PINB, PINB2) // IN1
+    SET_LOW(PINA, PINA7) // IN2
+
+    for (;;) {
+        // If the counter passes the limit, set the sleep
+        if (TCNT1 >= DEAD_TIME) {
+            // Enable sleep
+            SET_LOW(PINA, PINA4)
+        }
+
+        // Manage LEDs
+        // Check if sleep is enabled
+        if (REGISTER_BIT(PINA, PINA4)) {
+            // Turn off LEDs
+            SET_LOW(PINA, PINA3)
+            SET_LOW(PINA, PINA6)
+        }
+        else if (direction == 0) { // Neutral
+            // Turn on both LEDs
+            SET_HIGH(PINA, PINA3)
+            SET_HIGH(PINA, PINA6)
+        }
+        else {
+            // Choose the right pin
+            char led_pin;
+            char off_pin;
+            if (direction > 0) {
+                led_pin = PINA6;
+                off_pin = PINA3;
+            }
+            else {
+                led_pin = PINA3;
+                off_pin = PINA6;
+            }
+            SET_LOW(PINA, off_pin)
+            // Turn on if timer is below level (longer period for higher duty cycle PWM)
+            if (outer_time < compare_level) {
+                SET_HIGH(PINA, led_pin)
+            }
+            else {
+                SET_LOW(PINA, led_pin)
+            }
+        }
+    }
 
     return 0;
 }
@@ -58,26 +114,65 @@ int main() {
  * Calculates input pulse length.
  */
 ISR(PCINT0_vect) {
-    char pin_state = REGISTER_BIT(PINA, PINA0)
+    char pin_state = REGISTER_BIT(PINA, PINA0);
+    // Copy to prevent timer changing during calculation
+    unsigned short _time = TCNT1;
     // If pin moving LOW, new pulse beginning
     if (!pin_state) {
-        in_low_counts = 0;
-    }
-    // If pin moving HIGH, pulse ending
-    if (!pin_state) {
-        // Disable watchdog latch
-        input_off_latch = 0;
-        unsigned short pulse_length = 5 * in_low_counts;
+        TCNT1 = 0; // Reset timer
+    } // If pin moving HIGH, pulse ending
+    else {
+        // Verify this is a valid signal by making sure it's in the valid length
+        if (_time >= MIN_PULSE_LEN && _time <= MAX_PULSE_LEN) {
+            // Disable sleep
+            SET_HIGH(PINA, PINA4)
+
+            // REVERSE is PWM on IN2 (IN1 low)
+            // FWD is PWM on IN1 (IN2 low)
+
+            // Deadzone/neutral state
+            if (_time > DEADZONE_LOW && _time < DEADZONE_HIGH) {
+                // If brake pin is unset, use coast mode
+                char brake = ~REGISTER_BIT(PINA, PINA1);
+                SET_REGISTER(PINB, PINB2, brake)
+                SET_REGISTER(PINA, PINA7, brake)
+
+                // Set LEDs both on
+                SET_HIGH(PINA, PINA3)
+                SET_HIGH(PINA, PINA6)
+                direction = 0;
+            }
+            else {
+                // Calculate compare level
+                // Set to both OCR0A and OCR0B
+                // Enable or disable OC0A and OC0B as necessary
+                // Set LEDs
+                if (_time > DEADZONE_CENTER) {
+                    compare_level = (255 * (_time - DEADZONE_HIGH)) / IN_RANGE;
+                    // Set OC0A up
+                    SET_HIGH(TCCR0A, COM0A1)
+                    // Disable 0C0B
+                    SET_LOW(TCCR0A, COM0B1)
+
+                    direction = 1;
+                }
+                else {
+                    compare_level = (255 * (DEADZONE_LOW - _time)) / IN_RANGE;
+                    // Disable 0C0A
+                    SET_LOW(TCCR0A, COM0A1)
+                    // Set 0C0B up
+                    SET_HIGH(TCCR0A, COM0B1)
+
+                    direction = -1;
+                }
+                OCR0A = compare_level;
+                OCR0B = compare_level;
+            }
+        }
     }
 
 }
 
-/*
- * Keeps track of time to 5 uS resolution
- */
-ISR(TIM1_COMPA_vect) {
-    in_low_counts++;
-    if (in_low_counts >= DEAD_TIME) {
-        input_off_latch = 1;
-    }
+ISR(TIM1_OVF_vect) {
+    outer_time++;
 }
